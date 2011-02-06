73b3cd42a0e4204167432d8222c386024fa12190
Jeff Schumacher <jeff@codingreflection.com>
2011-02-06 13:03:16 -0800

    Initial commit

---
+++ src/Giles/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Giles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Giles - your own personal watcher");
+
+            Console.ReadKey();
+
+        }
+    }
+}
