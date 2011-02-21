4dcbf7a71636bd0f92e65b89c5eb47f78efa7bf5
codereflection <jeff@codingreflection.com>
2011-02-20 17:52:20 -0800

    Adding Giles.Runner.Machine.Specifications projects

---
+++ src/Runners/Giles.Machine.Specfications.Runner/MSpecRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Giles.Core.Runners;
+
+namespace Giles.Machine.Specfications.Runner
+{
+    public class MSpecRunner : IRunner
+    {
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
