bf042f5025dde4c0546a58407c03c38394d8d096
codereflection <jeff@codingreflection.com>
2011-02-21 08:49:52 -0800

    More work on Giles Test Listener

--- src/Giles.Core/Runners/ITestListener.cs
+++ src/Giles.Core/Runners/ITestListener.cs
@@ -3,7 +3,6 @@
     public interface ITestListener
     {
         void WriteLine(string text, string category);
-        void TestFinished(TestResult summary);
-        void TestResultsUrl(string resultsUrl);
+        void AddTestSummary(TestResult summary);
     }
 }
