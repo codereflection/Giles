523291a43dedef5143dacc0fb96a4c5d9e0e436c
codereflection <jeff@codingreflection.com>
2011-05-18 16:58:41 -0700

    Getting BuildRunner under test

---
+++ src/Giles.Specs/Core/Runners/FakeUserDisplay.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using Giles.Core.Runners;
+using Giles.Core.UI;
+
+namespace Giles.Specs.Core.Runners
+{
+    public class FakeUserDisplay : IUserDisplay
+    {
+        public IList<string> messagesReceived = new List<string>();
+        public IList<ExecutionResult> executionResultsReceived = new List<ExecutionResult>();
+
+        public void DisplayMessage(string message, params object[] parameters)
+        {
+            messagesReceived.Add(string.Format(message, parameters));
+        }
+
+        public void DisplayResult(ExecutionResult result)
+        {
+            executionResultsReceived.Add(result);
+        }
+    }
+}
