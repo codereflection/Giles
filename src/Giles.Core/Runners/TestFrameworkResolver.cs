59ede19778b6aa15e3b44b628baf5f3e11c17964
codereflection <jeff@codingreflection.com>
2011-05-24 07:34:08 -0700

    Changing TestFrameworkResolver to resolve TestFrameworkInspectors by reflection

--- src/Giles.Core/Runners/TestFrameworkResolver.cs
+++ src/Giles.Core/Runners/TestFrameworkResolver.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using Giles.Core.Utility;
@@ -7,7 +9,7 @@ namespace Giles.Core.Runners
 {
     public class TestFrameworkResolver
     {
-        List<TestFrameworkInspector> supportedFrameworkRunners;
+        List<TestFrameworkInspector> supportedFrameworkRunners = new List<TestFrameworkInspector>();
 
         public TestFrameworkResolver()
         {
@@ -16,11 +18,25 @@ namespace Giles.Core.Runners
 
         void BuildRunnerList()
         {
-            supportedFrameworkRunners = new List<TestFrameworkInspector>
-                                            {
-                                                new MSpecTestFrameworkInspector(),
-                                                new NUnitTestFrameworkInspector(),
-                                            };
+            var location = Path.GetDirectoryName(typeof(TestFrameworkResolver).Assembly.Location);
+
+            var files = Directory.GetFiles(location, "*.dll");
+
+            files.Each(file =>
+                           {
+                               var assembly = Assembly.LoadFrom(file);
+                               var types = assembly.GetTypes()
+                                                    .Where(type => type.IsSubclassOf(typeof(TestFrameworkInspector))).ToList();
+
+                               if (types.Count == 0)
+                                   return;
+
+                               types.Each(type =>
+                                              {
+                                                  Console.WriteLine("Found type {0}", type.FullName);
+                                                  supportedFrameworkRunners.Add(Activator.CreateInstance(type) as TestFrameworkInspector);
+                                              });
+                           });
         }
 
 
