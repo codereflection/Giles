02385064b02be0d1cc0dfaff18183c4e76957d38
codereflection <jeff@codingreflection.com>
2011-08-31 23:58:02 -0700

    Refactoring MSpecRunner

--- src/Runners/Giles.Runner.Machine.Specifications/MSpecRunner.cs
+++ src/Runners/Giles.Runner.Machine.Specifications/MSpecRunner.cs
@@ -9,41 +9,44 @@ namespace Giles.Runner.Machine.Specifications
 {
     public class MSpecRunner : IFrameworkRunner
     {
+        public IEnumerable<string> RequiredAssemblies()
+        {
+            return new[] { Assembly.GetAssembly(typeof(MSpecRunner)).Location, "ImpromptuInterface.dll" };
+        }
+
         public SessionResults RunAssembly(Assembly assembly)
         {
             var mspecAssembly = LoadMSpec(assembly);
-            var exportedTypes = mspecAssembly.GetExportedTypes();
-            
-            MSpecTypes.Types = exportedTypes;
-
-            var appDomainRunnerType = exportedTypes.First(x => x.Name == "AppDomainRunner");
-            var specificationRunListenerType = exportedTypes.First(x => x.Name == "ISpecificationRunListener");
-
-            var runOptionsType = exportedTypes.First(x => x.Name == "RunOptions");
+            MSpecTypes.Types = mspecAssembly.GetExportedTypes();
             
             var sessionResults = new SessionResults();
-            var testResults = new List<TestResult>();
-            var resultFormatterFactory = new ResultFormatterFactory();
-            
-            var dynamicRunListener = Impromptu.DynamicActLike(GilesMSpecRunListener.GetAnonymousListener(sessionResults, testResults, resultFormatterFactory),
-                                                     specificationRunListenerType);
-
-            var runOptions = Activator.CreateInstance(runOptionsType, new string[] { }, new string[] { }, new string[] { });
-
-            var runner = Activator.CreateInstance(appDomainRunnerType, dynamicRunListener, runOptions);
+            var runner = GetRunner(sessionResults);
 
             runner.RunAssembly(assembly);
             return sessionResults;
         }
 
+        private static dynamic GetRunner(SessionResults sessionResults)
+        {
+            dynamic dynamicRunListener = GetMSpecRunListener(sessionResults);
+
+            var runOptionsType = MSpecTypes.Types.First(x => x.Name == "RunOptions");
+            var runOptions = Activator.CreateInstance(runOptionsType, new string[] { }, new string[] { }, new string[] { });
+
+            var appDomainRunnerType = MSpecTypes.Types.First(x => x.Name == "AppDomainRunner");
+            return Activator.CreateInstance(appDomainRunnerType, dynamicRunListener, runOptions);
+        }
+
+        private static object GetMSpecRunListener(SessionResults sessionResults)
+        {
+            var specificationRunListenerType = MSpecTypes.Types.First(x => x.Name == "ISpecificationRunListener");
+            return Impromptu.DynamicActLike(GilesMSpecRunListener.GetAnonymousListener(sessionResults, new List<TestResult>(), new ResultFormatterFactory()),
+                                            specificationRunListenerType);
+        }
+
         private static Assembly LoadMSpec(Assembly assembly)
         {
             return Assembly.Load(assembly.GetReferencedAssemblies().FirstOrDefault(x => x.FullName.StartsWith("Machine.Specifications")));
-        }
-
-        public IEnumerable<string> RequiredAssemblies()
-        {
-            return new[] { Assembly.GetAssembly(typeof(MSpecRunner)).Location, "ImpromptuInterface.dll" };
         }
     }
 }
