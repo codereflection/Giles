0f2d771a4867fe4ead8a3f1364c4e2ed4ac9bc8d
codereflection <jeff@codingreflection.com>
2011-11-05 22:29:11 -0700

    misspelled Spidey. I suck.

--- src/Specs/Support/ClassLibraryWithTests/SpiderManTests.cs
+++ src/Specs/Support/ClassLibraryWithTests/SpiderManTests.cs
@@ -7,9 +7,9 @@ namespace ClassLibraryWithTests
         [Test]
         public void SpiderMan_should_know_his_own_catch_phrase()
         {
-            var spidy = new SpiderMan();
+            var spidey = new SpiderMan();
 
-            var thePhrase = spidy.SayCatchPhrase();
+            var thePhrase = spidey.SayCatchPhrase();
 
             Assert.That(thePhrase, Is.EqualTo("It's your Friendly Neighborhood Spider-Man!"));
         }
